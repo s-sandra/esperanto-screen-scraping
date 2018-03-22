4f5a05d0f170280ebcd8e8993e2694754f8d25c4
s-sandra <aleksandra.shtabnaya@gmail.com>
2018-03-22 09:07:22 -0400

    Parsed for root, style and color data for each word.

--- challenge2.py
+++ challenge2.py
@@ -6,8 +6,12 @@ from bs4 import BeautifulSoup
 soup = BeautifulSoup(open("esperanto.html", encoding="utf8"), "html.parser")
 
 # gets the data in the second table, containing common esperanto words
-word_data = soup.find_all("td")[10:]
+words_tds = soup.find_all("td")[10:]
 
-for word_cols in word_data:
-    for word in word_cols.ol:
-        print("one thing: ", word)
+for word_cols in words_tds:
+    words = word_cols.find_all("li")
+
+    for word in words:
+        root = word.get_text()
+        style = word.attrs # might have bold style.
+        color = word.a.attrs # might be red or green. Otherwise, blue.
