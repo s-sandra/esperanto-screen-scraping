f337e8af16ade7097698985629692ba2b39871f6
s-sandra <aleksandra.shtabnaya@gmail.com>
2018-03-21 22:34:45 -0400

    Updated html file. Parsed html file for elements containing word data.

--- challenge2.py
+++ challenge2.py
@@ -1,6 +1,13 @@
-# @authors Sandra Shtabnaya and Matthew Karp
+# @authors Sandra Shtabnaya and Matt Karp
 
 from bs4 import BeautifulSoup
 
 # parses esperanto.html using html parser
 soup = BeautifulSoup(open("esperanto.html", encoding="utf8"), "html.parser")
+
+# gets the data in the second table, containing common esperanto words
+word_data = soup.find_all("td")[10:]
+
+for word_cols in word_data:
+    for word in word_cols.ol:
+        print("one thing: ", word)
